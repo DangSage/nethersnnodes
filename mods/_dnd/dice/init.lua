892b00b2678da50c0919248fa644694829424b9e
Ethan Dang <3thandang@gmail.com>
2025-01-12 14:32:35 -0500

    dice setup

--- mods/_dnd/dice/init.lua
+++ mods/_dnd/dice/init.lua
@@ -5,14 +5,23 @@ dnd_dice = {}
 math.randomseed(os.time())
 
 di_type = {
-    D4 = 4,
-    D6 = 6,
-    D8 = 8,
-    D10 = 10,
-    D12 = 12,
-    D20 = 20
+    d4 = 4,
+    d6 = 6,
+    d8 = 8,
+    d10 = 10,
+    d12 = 12,
+    d20 = 20
 }
 
+local last_interaction = {}
+
+-- user options
+local show_roll_messages = minetest.settings:get_bool("dnd_dice.show_roll_messages", true)
+local show_last_roll = minetest.settings:get_bool("dnd_dice.show_last_roll", true)
+local roll_cooldown = tonumber(minetest.settings:get("dnd_dice.roll_cooldown")) or 2
+
+
+-- =================================== dice rolling
 -- function to generically roll as many dice as you want of any type of dice
 function dnd_dice.roll(dice_type, num_dice)
     local total = 0
@@ -26,11 +35,51 @@ function dnd_dice.roll(dice_type, num_dice)
     return total
 end
 
--- command to roll any number of any type of dice in the form /roll 2d6 or /roll 1d20 or variable number of dice /roll 3d4+2 or /roll 2d8+6d8
--- optionally, a name can be included in the form /roll <name> 2d6
+local hud_ids = {}
+
+function dnd_dice.roll_msg(name, dice, result)
+    if show_roll_messages then
+        minetest.chat_send_all(name .. " placed and rolled (" .. dice .. ") a " .. minetest.colorize("#ffa600", tostring(result)))
+    end
+end
+
+function dnd_dice.roll_hud(player, dice, result)
+    local name = player:get_player_name()
+    if player then
+        -- Remove previous HUD if it exists
+        if hud_ids[name] then
+            player:hud_remove(hud_ids[name])
+        end
+        -- Add new HUD
+        local hud_id = player:hud_add({
+            hud_elem_type = "text",
+            position = {x = 0.5, y = 0.5},
+            offset = {x = 0, y = 0},
+            text = "Last roll: " .. minetest.colorize("#ffa600", tostring(result)),
+            number = 0xFFFFFF,
+            alignment = {x = 0, y = 5},
+            scale = {x = 100, y = 100},
+            size = {x = 0, y = 0},
+            z_index = 0
+        })
+        hud_ids[name] = hud_id
+    end
+end
+
+minetest.register_on_leaveplayer(function(player)
+    local name = player:get_player_name()
+    if hud_ids[name] then
+        player:hud_remove(hud_ids[name])
+        hud_ids[name] = nil
+    end
+end)
+
+
+-- command to roll any number of a variable number of dice, with as many constants as you want
+-- for example, /roll 2d6 or /roll 1d20 or /roll 3d4+2 or /roll 2d8+6d8
 minetest.register_chatcommand("roll", {
     params = "[<name>] <dice>",
-    description = "Roll any number of any type of dice in the form /roll 2d6 or /roll 1d20 or variable number of dice /roll 3d4+2 or /roll 2d8+6d8. Optionally, include a name in the form /roll <name> 2d6",
+    description = "Roll any number of a variable number of dice, with optional constant parsing. Examples: /roll 2d6 or /roll 1d20 or /roll 3d4+2 or /roll 2d8+6d8",
     privs = {interact = true},
     func = function(name, param)
         local total = 0
@@ -45,7 +94,7 @@ minetest.register_chatcommand("roll", {
         end
 
         for num_dice, dice_type in dice_param:gmatch("(%d+)d(%d+)") do
-            local dice_key = "D" .. dice_type
+            local dice_key = "d" .. dice_type
             local result, err = dnd_dice.roll(dice_key, tonumber(num_dice))
 
             if result then
@@ -70,10 +119,64 @@ minetest.register_chatcommand("roll", {
 
         if valid then
             local message = roll_name .. " rolled " .. dice_param .. ": " .. result_string .. " = " .. minetest.colorize("#ffa600", tostring(total))
-            minetest.chat_send_all(message)
+            if show_roll_messages then
+                minetest.chat_send_all(message)
+            end
+            if show_last_roll then
+                dnd_dice.roll_hud(minetest.get_player_by_name(name), dice_param, total)
+            end
             return true
         else
             return false, "Invalid dice roll command"
         end
     end
 })
+
+
+
+-- =================================== dice nodes
+local _collision = {
+    type = "fixed",
+    fixed = {-0.28, -0.5, -0.28, 0.28, 0.0575, 0.28}
+}
+
+for dice, sides in pairs(di_type) do
+    minetest.register_node("dnd_dice:" .. dice, {
+        description = "DnD " .. dice,
+        drawtype = "mesh",
+        mesh = dice .. ".obj",
+        tiles = {"dice_base.png^[combine:128x128:0,0=dice_" .. sides .. ".png"},
+        collision_box = { type = "fixed", fixed = {0,0,0,0,0,0} },
+        selection_box = _collision,
+        paramtype = "light",
+        paramtype2 = "facedir",
+        light_source = 5,
+        groups = {oddly_breakable_by_hand = 3},
+        on_place = minetest.rotate_node,
+        on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+            local player_name = clicker:get_player_name()
+            local current_time = minetest.get_gametime()
+            
+            if last_interaction[player_name] and current_time - last_interaction[player_name] < roll_cooldown then
+                return  -- Prevent spamming
+            end
+            
+            local result, err = dnd_dice.roll(dice, 1)
+            if result then
+                dnd_dice.roll_msg(player_name, dice, result)
+                if show_last_roll then
+                    dnd_dice.roll_hud(clicker, dice, result)
+                end
+                last_interaction[player_name] = current_time
+                -- Update the node texture to show the result
+                minetest.swap_node(pos, {
+                    name = node.name,
+                    param2 = node.param2,
+                    tiles = {"dice_base.png^[combine:128x128:0,0=dice_" .. result .. ".png"}
+                })
+            else
+                minetest.chat_send_player(player_name, err)
+            end
+        end
+    })
+end
