f312147c949c94054cb30f74e0fc1b85bc5a6438
Ethan Dang <3thandang@gmail.com>
2025-04-24 13:04:10 -0400

    strafing allowed

--- mods/_player/sprint_lite/init.lua
+++ mods/_player/sprint_lite/init.lua
@@ -134,7 +134,7 @@ minetest.register_globalstep(function(dtime)
         end
 
         print(require_ground, playerstats.grounded, not require_ground)
-        if (keys.aux1 and keys.up and not keys.left and not keys.right and not keys.down and not keys.sneak) or 
+        if (keys.aux1 and not keys.down and not keys.sneak) or 
            (climbable and ((keys.aux1 and keys.sneak) or (keys.aux1 and keys.jump))) then
             if ((require_ground and playerstats.grounded) or not require_ground) and
                ((not playerstats.sprinting and playerstats.stamina > stamina_threshold) or 
